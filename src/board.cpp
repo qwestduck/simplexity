0de043d4ae98378b1034a81c4835a5f39791801b
William Panlener <wpanlener@gmail.com>
2012-11-25 23:23:07 -0600

    Add a piece to the pole when calling Pole::drop()

--- src/board.cpp
+++ src/board.cpp
@@ -2,9 +2,15 @@
 
 Pole::Pole() {
   _pieces = new piece_t[_MAXZ];
+  _z = 0;
 }
 
 status_t Pole::drop(piece_t piece) {
+  if(_z == _MAXZ) return STATUS_POLEFULL;
+
+  _pieces[_z] = piece;
+  _z++;
+
   return STATUS_OK;
 }
 
