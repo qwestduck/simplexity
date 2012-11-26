7612f0101650d37a97691a661548fdecfccb605c
William Panlener <wpanlener@gmail.com>
2012-11-25 23:11:14 -0600

    Pass responsibility for drop() from Board to Pole

--- src/board.cpp
+++ src/board.cpp
@@ -1,9 +1,13 @@
 #include "board.h"
 
+status_t Pole::drop(piece_t piece) {
+  return STATUS_OK;
+}
+
 Board::Board() {
 
 }
 
 status_t Board::drop(piece_t piece, int pole) {
-  return STATUS_OK;
+  return poles[pole].drop(piece);
 }
