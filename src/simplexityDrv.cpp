4e5d42e0d399f674d7f19da16282a525604cf2d7
William Panlener <wpanlener@gmail.com>
2012-11-30 19:38:39 -0600

    Add commandline flags to driver

--- src/simplexityDrv.cpp
+++ src/simplexityDrv.cpp
@@ -1,7 +1,33 @@
 #include "game.h"
+#include <string>
 
-int main() {
-  SvSGame g;
-  g.play();
+void usage() {
+  std::cout << "./simplexity <game_type>" << std::endl;
+  std::cout << std::endl;
+  std::cout << "  game_type:" << std::endl;
+  std::cout << "  -rvr for random vs random" << std::endl;
+  std::cout << "  -rvs for random vs smart" << std::endl;
+  std::cout << "  -svs for smart vs smart" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  RvRGame rvrg;
+  RvSGame rvsg;
+  SvSGame svsg;
+
+  if(argc != 2)
+    usage();
+  else {
+    std::string arg = argv[1];
+    if(arg == "-rvr")
+      rvrg.play();
+    else if(arg == "-rvs")
+      rvsg.play();
+    else if(arg == "-svs")
+      svsg.play();
+    else
+      usage();
+  }
+
   return 0;
 }
